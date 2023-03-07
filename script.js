bc7bb820cb219b32a78d6f28714c9e526a00186e
Egnoo CTE <84653206+MateoCollective@users.noreply.github.com>
2023-03-08 05:34:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // search meals function
-  $("#searchBtn").click(function() {
+  $("#searchBtn").click(function () {
     let query = $("#searchInput").val();
     if (query !== "") {
       let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + query;
-      $.getJSON(url, function(data) {
+      $.getJSON(url, function (data) {
         let meals = data.meals;
         if (meals) {
           $("#meals").empty();
-          meals.forEach(function(meal) {
+          meals.forEach(function (meal) {
             let mealCard = `
             <div class="search-meal-card" data-id="${meal.idMeal}">
             <div class="search-meal-info">
@@ -31,25 +31,36 @@ $(document).ready(function() {
   });
 
   // get meal details function
-  $("#meals").on("click", ".search-meal-card", function() {
+  $("#meals").on("click", ".search-meal-card", function () {
     let mealId = $(this).data("id");
     let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function (data) {
       let meal = data.meals[0];
       let mealDetails = `
-        <div class="meal-details">
-        <button class="close-btn">Close</button>
-          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-          <h2>${meal.strMeal}</h2>
-          <h3>Ingredients:</h3>
-          <ul>
-            ${getIngredients(meal)}
-          </ul>
-          <h3>Instructions:</h3>
-          <p>${meal.strInstructions}</p>
-        </div>
+      <div class="header gradient-header">
+      <div class="left">
+        <a href="#"><i class="fas fa-arrow-left close-btn"></i></a>
+      </div>
+      <div class="center-text">
+      <span>${meal.strMeal}</span>
+      </div>
+      <div class="right">
+        <a href="#"><i class="fas fa-ellipsis-v"></i></a>
+      </div>
+    </div>
+    
+    <div class="meal-details">
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+      <h2>${meal.strMeal}</h2>
+      <h3>Ingredients:</h3>
+      <ul>
+        ${getIngredients(meal)}
+      </ul>
+      <h3>Instructions:</h3>
+      <a>${meal.strInstructions}</a>
+    </div>
       `;
-      $("#meals").fadeOut(500, function() {
+      $("#meals").fadeOut(500, function () {
         $(this).empty().append(mealDetails).fadeIn(500);
       });
     });
@@ -61,7 +72,7 @@ $(document).ready(function() {
     for (let i = 1; i <= 20; i++) {
       if (meal["strIngredient" + i]) {
         ingredients += `
-          <a>${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}</a>
+          <li>${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}</li>
         `;
       } else {
         break;
@@ -71,12 +82,229 @@ $(document).ready(function() {
   }
 
   // add event listener to close button
-  $("#meals").on("click", ".close-btn", function() {
-    $("#meals").fadeOut(500, function() {
+  $("#meals").on("click", ".close-btn", function () {
+    $("#meals").fadeOut(500, function () {
       $(this).empty().show();
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getMealData() {
+  $.getJSON("https://www.themealdb.com/api/json/v1/1/random.php", function (data) {
+    const meal = data.meals[0];
+    const imageUrl = meal.strMealThumb;
+    const title = meal.strMeal;
+    const description = meal.strInstructions;
+
+    $(".slider-image").css("background-image", "url(" + imageUrl + ")");
+    $(".slider-title").text(title);
+    $(".slider-description").text(description);
+  });
+}
+
+getMealData();
+setInterval(getMealData, 5000);
+
+
+$(document).ready(function () {
+  // get meal details function
+  $("#random-meals").on("click", ".meal-card", function () {
+    let mealId = $(this).data("id");
+    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
+    $.getJSON(url, function (data) {
+      let meal = data.meals[0];
+      let mealDetails = `
+      <div class="header gradient-header">
+            <div class="left">
+              <a href="#"><i class="fas fa-arrow-left close-btn"></i></a>
+            </div>
+            <div class="center-text">
+            <span>${meal.strMeal}</span>
+            </div>
+            <div class="right">
+              <a href="#"><i class="fas fa-ellipsis-v"></i></a>
+            </div>
+          </div>
+          
+          <div class="meal-details">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <h2>${meal.strMeal}</h2>
+            <h3>Ingredients:</h3>
+            <ul>
+              ${getIngredients(meal)}
+            </ul>
+            <h3>Instructions:</h3>
+            <a>${meal.strInstructions}</a>
+          </div>
+      `;
+      $("#random-meals").fadeOut(500, function () {
+        $(this).empty().append(mealDetails).fadeIn(500);
+      });
+    });
+  });
+
+  // helper function to get ingredients
+  function getIngredients(meal) {
+    let ingredients = "";
+    for (let i = 1; i <= 20; i++) {
+      if (meal["strIngredient" + i]) {
+        ingredients += `
+          <li>${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}</li>
+        `;
+      } else {
+        break;
+      }
+    }
+    return ingredients;
+  }
+
+  // add event listener to close button
+  $("#random-meals").on("click", ".close-btn", function () {
+    $("#random-meals").fadeOut(500, function () {
+      $(this).empty().show();
+    });
+  });
+
+  // get random meals and display them
+  let url = "https://www.themealdb.com/api/json/v1/1/random.php";
+  let numOfMeals = 6; // set the number of random meals to display
+  for (let i = 0; i < numOfMeals; i++) {
+    $.getJSON(url, function (data) {
+      let meal = data.meals[0];
+      let mealCard = `
+      
+      <div class="meal-container">
+        <div class="meal-card" data-id="${meal.idMeal}">
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <div class="meal-desk">
+          <h3>${meal.strMeal}</h3>
+          <p> ${meal.strCategory}</p>
+        </div>
+        </div>
+        </div>
+      `;
+      $("#random-meals").append(mealCard);
+    });
+  }
+});
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+  // get meal details function
+  $("#jepang-meals").on("click", ".meal-card", function () {
+    let mealId = $(this).data("id");
+    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
+    $.getJSON(url, function (data) {
+      let meal = data.meals[0];
+      let mealDetails = `
+      <div class="header gradient-header">
+            <div class="left">
+              <a href="#"><i class="fas fa-arrow-left close-btn"></i></a>
+            </div>
+            <div class="center-text">
+            <span>${meal.strMeal}</span>
+            </div>
+            <div class="right">
+              <a href="#"><i class="fas fa-ellipsis-v"></i></a>
+            </div>
+          </div>
+          
+          <div class="meal-details">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <h2>${meal.strMeal}</h2>
+            <h3>Ingredients:</h3>
+            <ul>
+              ${getIngredients(meal)}
+            </ul>
+            <h3>Instructions:</h3>
+            <a>${meal.strInstructions}</a>
+          </div>
+      `;
+      $("#jepang-meals").fadeOut(500, function () {
+        $(this).empty().append(mealDetails).fadeIn(500);
+      });
+    });
+  });
+
+  // helper function to get ingredients
+  function getIngredients(meal) {
+    let ingredients = "";
+    for (let i = 1; i <= 20; i++) {
+      if (meal["strIngredient" + i]) {
+        ingredients += `
+          <li>${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}</li>
+        `;
+      } else {
+        break;
+      }
+    }
+    return ingredients;
+  }
+
+  // add event listener to close button
+  $("#jepang-meals").on("click", ".close-btn", function () {
+    $("#jepang-meals").fadeOut(500, function () {
+      $(this).empty().show();
+    });
+  });
+
+  // get random Japanese meals and display them
+  let url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese";
+  let numOfMeals = 6; // set the number of random meals to display
+  $.getJSON(url, function (data) {
+    let meals = data.meals;
+    for (let i = 0; i < numOfMeals; i++) {
+      let randomIndex = Math.floor(Math.random() * meals.length);
+      let meal = meals[randomIndex];
+      let mealCard = `
+        <div class="meal-container">
+          <div class="meal-card" data-id="${meal.idMeal}">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <div class="meal-desk">
+              <h3>${meal.strMeal}</h3>y
+            </div>
+          </div>
+        </div>
+      `;
+      $("#jepang-meals").append(mealCard);
+      meals.splice(randomIndex, 1); // remove the chosen meal from the array
+    }
+  });
+});
+
+
 
 
 
@@ -162,7 +390,7 @@ function toggleDarkMode() {
 document.addEventListener('DOMContentLoaded', function () {
   setTimeout(function () {
     document.getElementById('splash-screen').style.display = 'none';
-  }, 000); //ganti nilai 3000 menjadi berapa lama Anda ingin splash screen tampil (dalam milidetik)
+  }, 3000); //ganti nilai 3000 menjadi berapa lama Anda ingin splash screen tampil (dalam milidetik)
 });
 
 
@@ -208,107 +436,6 @@ tabs.forEach((tab) => {
   });
 });
 
-function getMealData() {
-  $.getJSON("https://www.themealdb.com/api/json/v1/1/random.php", function (data) {
-    const meal = data.meals[0];
-    const imageUrl = meal.strMealThumb;
-    const title = meal.strMeal;
-    const description = meal.strInstructions;
-
-    $(".slider-image").css("background-image", "url(" + imageUrl + ")");
-    $(".slider-title").text(title);
-    $(".slider-description").text(description);
-  });
-}
-
-getMealData();
-setInterval(getMealData, 5000);
-
-
-$(document).ready(function() {
-  // get meal details function
-  $("#random-meals").on("click", ".meal-card", function() {
-    let mealId = $(this).data("id");
-    let url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
-    $.getJSON(url, function(data) {
-      let meal = data.meals[0];
-      let mealDetails = `
-      <header class="header gradient-header">
-            <div class="left">
-              <a href="#"><i class="fas fa-arrow-left close-btn"></i></a>
-            </div>
-            <div class="center-text">
-            <span>${meal.strMeal}</span>
-            </div>
-            <div class="right">
-              <a href="#"><i class="fas fa-ellipsis-v"></i></a>
-            </div>
-          </header>
-          
-        <div class="meal-details">
-          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-          <h2>${meal.strMeal}</h2>
-          <h3>Ingredients:</h3>
-          <p>
-            ${getIngredients(meal)}
-          </p>
-          <h3>Instructions:</h3>
-          <a>${meal.strInstructions}</a>
-          <button class="close-btn">Close</button>
-        </div>
-      `;
-      $("#random-meals").fadeOut(500, function() {
-        $(this).empty().append(mealDetails).fadeIn(500);
-      });
-    });
-  });
-
-  // helper function to get ingredients
-  function getIngredients(meal) {
-    let ingredients = "";
-    for (let i = 1; i <= 20; i++) {
-      if (meal["strIngredient" + i]) {
-        ingredients += `
-          <li>${meal["strIngredient" + i]} - ${meal["strMeasure" + i]}</li>
-        `;
-      } else {
-        break;
-      }
-    }
-    return ingredients;
-  }
-
-  // add event listener to close button
-  $("#random-meals").on("click", ".close-btn", function() {
-    $("#random-meals").fadeOut(500, function() {
-      $(this).empty().show();
-    });
-  });
-
-  // get random meals and display them
-  let url = "https://www.themealdb.com/api/json/v1/1/random.php";
-  let numOfMeals = 6; // set the number of random meals to display
-  for (let i = 0; i < numOfMeals; i++) {
-    $.getJSON(url, function(data) {
-      let meal = data.meals[0];
-      let mealCard = `
-      
-
-      <div class="meal-container">
-        <div class="meal-card" data-id="${meal.idMeal}">
-          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-          <div class="meal-desk">
-          <h3>${meal.strMeal}</h3>
-          <p> ${meal.strCategory}</p>
-        </div>
-        </div>
-        </div>
-      `;
-      $("#random-meals").append(mealCard);
-    });
-  }
-});
-
 
 // Fungsi untuk menampilkan ucapan selamat berdasarkan waktu
 function showGreeting() {
@@ -336,7 +463,7 @@ function showGreeting() {
 setInterval(showGreeting, 1000);
 
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   const header = document.querySelector('.header');
   if (window.pageYOffset > 0) {
     header.classList.add('header-scroll');
@@ -345,7 +472,7 @@ window.addEventListener('scroll', function() {
   }
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   const header = document.querySelector('.header');
   if (window.pageYOffset > 0) {
     header.classList.add('header-scroll');
